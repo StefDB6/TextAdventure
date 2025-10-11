716030aafbc224f084461242f0979c7901f6a274
Thomas Delbrouck (s130142) <thomas.delbrouck@student.ap.be>
2025-10-11 12:54:40 +0200

    item.cs and test project

---
+++ TestRaiders_TextAdventure/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRaiders_TextAdventure
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Item(string id, string name, string description)
+        {
+            //Id mag niet leeg of null zijn.
+            if (string.IsNullOrWhiteSpace(id))
+                throw new ArgumentException("Item Id mag niet leeg zijn.", nameof(id));
+
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+
+        public override string ToString() => $"{Name} ({Id}) - {Description}";
+    }
+}
---
+++ Tests/ItemTests.cs
@@ -0,0 +1,56 @@
+﻿using TestRaiders_TextAdventure;
+namespace Tests
+{
+    [TestClass]
+    public class ItemTests
+    {
+        [TestMethod]
+        //werkt de constructor wanneer je een Item object aanmaakt
+        public void Constuctor_Sets_All_Properties()
+        {
+            var item = new Item("key", "Sleutel", "Opent een deur");
+
+            Assert.AreEqual("key", item.Id);
+            Assert.AreEqual("Sleutel", item.Name);
+            Assert.AreEqual("Opent een deur", item.Description);
+        }
+
+        [TestMethod]
+
+        //props zijn niet read-only dus we moeten testen of deze nog wijzigbaar zijn
+        public void Properties_Are_Mutable() 
+        {
+            var item = new Item("sword", "Zwaard", "Tegen monsters");
+
+            item.Name = "Stalen zwaard";
+            item.Description = "Scherp";
+
+            Assert.AreEqual("Stalen zwaard", item.Name);
+            Assert.AreEqual("Scherp", item.Description);
+        }
+
+        [TestMethod]
+        // Test dat de ToString()-methode van Item een leesbare tekst teruggeeft 
+        // die minstens de naam en het ID van het item bevat.
+        // Zo weten we dat de methode nuttige informatie toont voor de speler.
+        public void ToString_Contains_Name_And_Id()
+        {
+            var item = new Item("key", "Sleutel", "Opent deur");
+            var s = item.ToString();
+
+            StringAssert.Contains(s, "Sleutel");
+            StringAssert.Contains(s, "key");
+        }
+
+        [TestMethod]
+        // we verwachten dat een lege Id wordt geweigerd.
+        // Bij fout: ArgumentException.
+        public void Id_Should_Not_Be_Null_Or_Empty()
+        {
+            // we verwachten dat een lege Id wordt geweigerd.
+            // Bij fout: ArgumentException.
+            Assert.ThrowsException<ArgumentException>(() => new Item("", "X", "Y"));
+            Assert.ThrowsException<ArgumentException>(() => new Item(null!, "X", "Y"));
+        }
+    }
+}
