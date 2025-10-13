09b55c7d8b3293efc4c074277f68b8fb766070ca
ThomasDelbrouck1 <s130142@ap.be>
2025-10-13 18:37:11 +0200

    item DI implementation

---
+++ TestRaiders_TextAdventure/Iitem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRaiders_TextAdventure
+{
+    // Publieke interface voor een spel-item.
+    public interface Iitem
+    {
+        string Id { get; }
+        string Name { get; set; }
+        string Description { get; set; }
+
+        string ToString();
+    }
+}
