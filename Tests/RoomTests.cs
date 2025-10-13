8f2c72bccdb4637b58d2c34a8ca244e1d33f7814
DVdBdev <dries.van.den.brande@proton.me>
2025-10-13 20:57:20 +0200

    - Room Interface - Room class - Room tests

---
+++ Tests/RoomTests.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestRaiders_TextAdventure;
+using TestRaiders_TextAdventure.Core.Interfaces;
+using TestRaiders_TextAdventure.Core.Models;
+
+namespace Tests
+{
+    [TestClass]
+    public class RoomTests
+    {
+        private Room _room;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _room = new Room("Hall", "A dark hallway.");
+        }
+
+        [TestMethod]
+        // Checks if the constructor correctly sets properties
+        public void Constructor_Sets_Properties_Correctly()
+        {
+            Assert.AreEqual("Hall", _room.Name);
+            Assert.AreEqual("A dark hallway.", _room.Description);
+            Assert.IsFalse(_room.IsDeadly);
+            Assert.IsFalse(_room.RequiresKey);
+            Assert.IsFalse(_room.HasMonster);
+        }
+
+        [TestMethod]
+        // Tests if exits can be added and retrieved
+        public void AddExit_And_GetExit_Works()
+        {
+            var nextRoom = new Room("Library", "Full of books.");
+            _room.AddExit(Direction.North, nextRoom);
+
+            var result = _room.GetExit(Direction.North);
+
+            Assert.AreEqual(nextRoom, result);
+        }
+
+        [TestMethod]
+        // Tests if items can be added to the room
+        public void AddItem_Adds_Item_To_Room()
+        {
+            var item = new Item("Key", "A shiny key");
+            _room.AddItem(item);
+
+            var items = _room.GetItems().ToList();
+
+            Assert.AreEqual(1, items.Count);
+            Assert.AreEqual("Key", items[0].Name);
+        }
+
+        [TestMethod]
+        // Tests if TakeItem removes and returns the correct item
+        public void TakeItem_Removes_And_Returns_Item()
+        {
+            var item = new Item("Sword", "A sharp weapon");
+            _room.AddItem(item);
+
+            var result = _room.TakeItem(item.Id);
+
+            Assert.AreEqual(item, result);
+            Assert.AreEqual(0, _room.GetItems().Count());
+        }
+
+        [TestMethod]
+        // Tests that TakeItem returns null if the item does not exist
+        public void TakeItem_Returns_Null_When_Item_Not_Found()
+        {
+            var result = _room.TakeItem("does_not_exist");
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        // Tests that the MonsterAlive flag works as expected
+        public void MonsterAlive_Can_Be_Updated()
+        {
+            var monsterRoom = new Room("Basement", "A creepy basement.", hasMonster: true);
+            Assert.IsTrue(monsterRoom.MonsterAlive);
+
+            monsterRoom.MonsterAlive = false;
+            Assert.IsFalse(monsterRoom.MonsterAlive);
+        }
+
+        [TestMethod]
+        // Test that a deadly room is correctly marked as deadly
+        public void Room_Can_Be_Deadly()
+        {
+            var deadlyRoom = new Room("Trap Room", "Spikes everywhere.", isDeadly: true);
+
+            Assert.IsTrue(deadlyRoom.IsDeadly);
+            Assert.IsFalse(deadlyRoom.RequiresKey);
+            Assert.IsFalse(deadlyRoom.HasMonster);
+        }
+
+        [TestMethod]
+        // Test that a locked room requires a key
+        public void Room_Can_Be_Locked()
+        {
+            var lockedRoom = new Room("Treasure Room", "The door is locked.", requiresKey: true);
+
+            Assert.IsTrue(lockedRoom.RequiresKey);
+            Assert.IsFalse(lockedRoom.IsDeadly);
+            Assert.IsFalse(lockedRoom.HasMonster);
+        }
+
+        [TestMethod]
+        // Test that a monster room initializes with MonsterAlive = true
+        public void Room_With_Monster_Starts_With_Alive_Monster()
+        {
+            var monsterRoom = new Room("Cave", "A monster lurks inside.", hasMonster: true);
+
+            Assert.IsTrue(monsterRoom.HasMonster);
+            Assert.IsTrue(monsterRoom.MonsterAlive);
+        }
+
+        [TestMethod]
+        // Test combination: locked and deadly room
+        public void Room_Can_Be_Locked_And_Deadly()
+        {
+            var trickyRoom = new Room("Vault", "A deadly trap behind a locked door.", isDeadly: true, requiresKey: true);
+
+            Assert.IsTrue(trickyRoom.IsDeadly);
+            Assert.IsTrue(trickyRoom.RequiresKey);
+            Assert.IsFalse(trickyRoom.HasMonster);
+        }
+
+        [TestMethod]
+        // Test combination: deadly monster room
+        public void Room_Can_Be_Deadly_And_Have_Monster()
+        {
+            var bossRoom = new Room("Lair", "The final boss awaits.", isDeadly: true, hasMonster: true);
+
+            Assert.IsTrue(bossRoom.IsDeadly);
+            Assert.IsTrue(bossRoom.HasMonster);
+            Assert.IsTrue(bossRoom.MonsterAlive);
+        }
+
+        [TestMethod]
+        // Test full combination: deadly, locked, monster room
+        public void Room_Can_Combine_All_Booleans()
+        {
+            var finalRoom = new Room(
+                "End Chamber",
+                "A locked, deadly room with a monster guarding the treasure.",
+                isDeadly: true,
+                requiresKey: true,
+                hasMonster: true
+            );
+
+            Assert.IsTrue(finalRoom.IsDeadly);
+            Assert.IsTrue(finalRoom.RequiresKey);
+            Assert.IsTrue(finalRoom.HasMonster);
+            Assert.IsTrue(finalRoom.MonsterAlive);
+        }
+    }
+}
