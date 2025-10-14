94a1faa32dcfb1a526e27856642a24f1173c4144
ThomasDelbrouck1 <s130142@ap.be>
2025-10-14 17:08:45 +0200

    item types added and implementation

---
+++ TestRaiders_TextAdventure/ItemType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRaiders_TextAdventure
+{
+    // item type in the game.
+    public enum ItemType
+    {
+        Key,
+        Sword,
+        Shield
+    }
+}
